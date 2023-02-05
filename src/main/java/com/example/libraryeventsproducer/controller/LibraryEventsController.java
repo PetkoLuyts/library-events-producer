4053a3d28a32875b413236324f0bd37c058997b2
petko.lyutskanov <simona2000petko@abv.bg>
2023-02-05 17:08:23 +0200

    Inject the LibraryEventProducer into the controller

--- src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
+++ src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
@@ -1,6 +1,9 @@
 package com.example.libraryeventsproducer.controller;
 
 import com.example.libraryeventsproducer.domain.LibraryEvent;
+import com.example.libraryeventsproducer.producer.LibraryEventProducer;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LibraryEventsController {
 
-    @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    @Autowired
+    LibraryEventProducer libraryEventProducer;
 
+    @PostMapping("/v1/libraryevent")
+    public ResponseEntity<LibraryEvent> postLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
+        libraryEventProducer.sendLibraryEvent(libraryEvent);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
     }
