5badd07de489ac233c9ca4de705d614cd190f163
petko.lyutskanov <simona2000petko@abv.bg>
2023-02-10 17:47:56 +0200

    Implement unit MockMVC test for put endpoint

--- src/test/java/unit/com/example/libraryeventsproducer/controller/LibraryEventsControllerUnitTest.java
+++ src/test/java/unit/com/example/libraryeventsproducer/controller/LibraryEventsControllerUnitTest.java
@@ -16,6 +16,7 @@ import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -75,5 +76,28 @@ public class LibraryEventsControllerUnitTest {
                         .contentType(MediaType.APPLICATION_JSON))
                         .andExpect(status().is4xxClientError())
                         .andExpect(content().string(expectedErrorMessage));
+    }
+
+    @Test
+    void updateLibraryEvent() throws Exception {
+        Book book = Book.builder()
+                .bookId(123)
+                .bookAuthor("Petko")
+                .bookName("Kafka Spring")
+                .build();
+
+        LibraryEvent libraryEvent = LibraryEvent.builder()
+                .libraryEventId(123)
+                .book(book)
+                .build();
+
+        String json = objectMapper.writeValueAsString(libraryEvent);
+        when(libraryEventProducer.sendLibraryEventApproach2(isA(LibraryEvent.class))).thenReturn(null);
+
+        mockMvc.perform(
+                        put("/v1/libraryevent")
+                                .content(json)
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 }
